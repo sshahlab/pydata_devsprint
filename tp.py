f25a2c4985ac51598f367691ca1d702f86bb17da
Saumya456 <saumyashah456@yahoo.com>
2020-02-02 10:29:40 +0530

    new

---
+++ tp.py
@@ -0,0 +1,11 @@
+handle = open('mail.txt')
+counts=dict()
+for line in handle:
+	if not line.startswith('From'): continue
+	l=line.split()
+	if len(l)<3:continue
+	t=l[len(l)-2][0:2]
+	counts[t]=counts.get(t,0)+1
+lst=sorted(counts.items())
+for v,k in lst[:]:
+	print(v,k)
